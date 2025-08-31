3caf56c63850fad55b6a7da9badd4e0494849bbd
LumenaRegina <223014400+LumenaRegina@users.noreply.github.com>
2025-08-31 12:22:43 +0200

    add grid

---
+++ scipt.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container")
+
+
+
+let x = 16
+let y = 16
+
+for (i = 0; i<y; i++) {
+    const row = document.createElement("div")
+    row.className = "row"
+    container.appendChild(row)
+    for (k=0; k<x; k++){
+        const square = document.createElement("div")
+        square.className = "square"
+        row.appendChild(square)
+    }
+}
+
+container.addEventListener("mouseover", () => {
+    event.target.classList.add("hovered")
+})
