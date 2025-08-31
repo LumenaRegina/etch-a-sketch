9ae6afdd9c57807a6362cd5a69fc38c025a1b660
LumenaRegina <223014400+LumenaRegina@users.noreply.github.com>
2025-08-31 16:37:38 +0200

    Add grid resize + UI

--- scipt.js
+++ scipt.js
@@ -1,21 +1,42 @@
 const container = document.querySelector(".container")
+const btn = document.querySelector("button")
+const body = document.querySelector("body")
+const grid = document.querySelector(".grid")
+const para = document.querySelector("p")
 
 
 
-let x = 16
-let y = 16
-
-for (i = 0; i<y; i++) {
+function drawGrid(x=16,y=x, target=document.querySelector(".grid")){
+    if (x>100) {
+        x = 100
+        y = 100
+    }
+    para.textContent = "Grid size: "+x+" x "+y
+    for (i = 0; i<y; i++) {
     const row = document.createElement("div")
     row.className = "row"
-    container.appendChild(row)
+    target.appendChild(row)
     for (k=0; k<x; k++){
         const square = document.createElement("div")
         square.className = "square"
         row.appendChild(square)
     }
+
+}
 }
 
 container.addEventListener("mouseover", (event) => {
     event.target.classList.add("hovered")
+})
+
+drawGrid()
+
+btn.addEventListener("click", () => {
+    const grid = document.querySelector(".grid")
+    grid.remove()
+    const newGrid = document.createElement("div")
+    newGrid.className = "grid"
+    container.appendChild(newGrid)
+    drawGrid(prompt("Enter grid length/height", "16"))
+
 })
